93f1ff2c2bbd76246068dd00b0a6e155e50fd6be
Muhammad Nabeel Asif <nabeel.asif362@gmail.com>
2025-09-25 23:34:58 +0500

    Add and implement session guard

--- src/modules/plans/plans.controller.ts
+++ src/modules/plans/plans.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { PlansService } from './plans.service';
+import { SessionGuard } from 'src/guards/session.guard';
 
 @Controller('plans')
 export class PlansController {
   constructor(private readonly plansService: PlansService) {}
 
+  @UseGuards(SessionGuard)
   @Get()
   async findAll() {
     return this.plansService.getAllPlans();

8c97e9329a7d6a7da63e461eb4395261119b1684
Muhammad Nabeel Asif <nabeel.asif362@gmail.com>
2025-09-30 00:47:27 +0500

    Include new schemas

--- src/database/schema/index.ts
+++ src/database/schema/index.ts
@@ -1,3 +1,5 @@
 export * from './user.schema';
 export * from './user_session.schema';
 export * from './billing_plan.schema';
+export * from './user_oauth_account.schema';
+export * from './contact_inquiry.schema';

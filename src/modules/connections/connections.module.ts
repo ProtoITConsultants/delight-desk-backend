8c00c2e3080663a5c358cbe2210acd11d3c54925
Muhammad Nabeel Asif <nabeel.asif362@gmail.com>
2025-10-14 00:31:58 +0500

    Add MicrosoftOauthRepository to connections module

--- src/modules/connections/connections.module.ts
+++ src/modules/connections/connections.module.ts
@@ -3,10 +3,11 @@ import { ConnectionsService } from './connections.service';
 import { ConnectionsController } from './connections.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { GoogleOauthRepository } from '../google-oauth/google-oauth.repository';
+import { MicrosoftOauthRepository } from '../microsoft-oauth/microsoft-oauth.repository';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [ConnectionsController],
-  providers: [ConnectionsService, GoogleOauthRepository],
+  providers: [ConnectionsService, GoogleOauthRepository, MicrosoftOauthRepository],
 })
 export class ConnectionsModule {}

e5a188acc78a9d79fa004b933f2849330be0c344
Muhammad Nabeel Asif <nabeel.asif362@gmail.com>
2025-10-14 00:32:16 +0500

    Add MicrosoftOauthRepository to connections module

--- src/modules/connections/connections.service.ts
+++ src/modules/connections/connections.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { GoogleOauthRepository } from '../google-oauth/google-oauth.repository';
+import { MicrosoftOauthRepository } from '../microsoft-oauth/microsoft-oauth.repository';
 
 @Injectable()
 export class ConnectionsService {
-  constructor(private readonly googleOauthRepo: GoogleOauthRepository) {}
+  constructor(
+    private readonly googleOauthRepo: GoogleOauthRepository,
+    private readonly msOauthRepo: MicrosoftOauthRepository,
+  ) {}
 
   async getConnectionsDetail(userId: string) {
     const gmailConnectionDetail = await this.googleOauthRepo.getGoogleAccount(userId);
+    const outlookConnectionDetail = await this.msOauthRepo.getMicrosoftAccount(userId);
+
     const connectionsDetailTemplate = {
       wooCommerce: null,
       gmail: gmailConnectionDetail
         ? { status: gmailConnectionDetail.status, email: gmailConnectionDetail.email }
         : null,
-      outlook: null,
+      outlook: outlookConnectionDetail
+        ? { status: outlookConnectionDetail.status, email: outlookConnectionDetail.email }
+        : null,
       shipbob: null,
       shipstation: null,
     };

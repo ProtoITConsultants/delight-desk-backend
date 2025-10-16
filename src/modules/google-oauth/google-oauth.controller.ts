fac8e9d87e4731e56fea482cdd4b067757cc3355
Muhammad Nabeel Asif <nabeel.asif362@gmail.com>
2025-10-16 21:46:59 +0500

    U

--- src/modules/google-oauth/google-oauth.controller.ts
+++ src/modules/google-oauth/google-oauth.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Delete, Get, NotFoundException, Req, Res, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GoogleOauthService } from './google-oauth.service';
 import { SessionGuard } from 'src/guards/session.guard';
 import { CurrentUserId } from 'src/decorators/current-user.decorator';
+import { ConfigService } from '@nestjs/config';
+import express from 'express';
 
 @Controller('google-oauth')
 export class GoogleOauthController {
-  constructor(private readonly googleService: GoogleOauthService) {}
+  constructor(
+    private readonly googleService: GoogleOauthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('login')
   @UseGuards(AuthGuard('google'))
@@ -14,7 +19,7 @@ export class GoogleOauthController {
 
   @Get('callback')
   @UseGuards(AuthGuard('google'))
-  async googleCallback(@Req() req: any, @Res() res: any) {
+  async googleCallback(@Req() req: any, @Res() res: express.Response) {
     const userId = req.session?.userId;
     const googleAccount = req.user;
     const scopes = req.query.scope?.toString().split(' ') || [];
@@ -23,14 +28,24 @@ export class GoogleOauthController {
       return res.status(401).send('You must be logged in to delight desk before connecting Google');
     }
 
+    const origin: string = req.get('origin') || `${req.protocol}://${req.get('host')}`;
+    const isRequestFromLocal = origin.includes('localhost');
+    const GOOGLE_SUCCESS_LOCAL_REDIRECT = this.configService.get('GOOGLE_SUCCESS_LOCAL_REDIRECT');
+    const GOOGLE_SUCCESS_STAGING_REDIRECT = this.configService.get(
+      'GOOGLE_SUCCESS_STAGING_REDIRECT',
+    );
+    const seeOtherStatusCode = 303;
+
     try {
       await this.googleService.connectGoogleAccount(userId, googleAccount, scopes);
-      const origin = req.get('origin') || `${req.protocol}://${req.get('host')}`;
-      return res.redirect(`${origin}/connections`);
+
+      return isRequestFromLocal
+        ? res.redirect(GOOGLE_SUCCESS_LOCAL_REDIRECT)
+        : res.redirect(GOOGLE_SUCCESS_STAGING_REDIRECT);
     } catch (err) {
-      console.error({ err });
-      const origin = req.get('origin') || `${req.protocol}://${req.get('host')}`;
-      return res.redirect(`${origin}/login`);
+      return isRequestFromLocal
+        ? res.redirect(seeOtherStatusCode, GOOGLE_SUCCESS_LOCAL_REDIRECT)
+        : res.redirect(seeOtherStatusCode, GOOGLE_SUCCESS_STAGING_REDIRECT);
     }
   }
 

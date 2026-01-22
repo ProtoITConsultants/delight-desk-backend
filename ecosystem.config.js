4f4a6cc5e086bdae0455690520d1d42343d43f00
Muhammad Nabeel Asif <nabeel.asif362@gmail.com>
2026-01-22 22:22:39 +0500

    Update ecosystem configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: 'delight-desk',
       script: 'dist/main.js',
-      instances: 'max',
-      exec_mode: 'cluster',
+      instances: 1,
+      exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
@@ -14,13 +14,15 @@ module.exports = {
       out_file: 'logs/delight-desk-out.log',
       time: true,
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
-
       max_memory_restart: '1G',
       autorestart: true,
       watch: false,
       max_restarts: 10,
       min_uptime: '10s',
       merge_logs: true,
+
+      kill_timeout: 5000,
+      listen_timeout: 3000,
     },
   ],
 };
